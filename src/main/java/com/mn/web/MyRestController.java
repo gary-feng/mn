9471d71b58e90b4e94bf31ad9f1fa27326ad5016
冯国军 <fgz@data-god.com>
2017-04-09 18:42:26 +0800

    Add jsp support.

--- src/main/java/com/mn/web/MyRestController.java
+++ src/main/java/com/mn/web/MyRestController.java
@@ -1,19 +1,26 @@
 package com.mn.web;
 
 import com.mn.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by FGJ on 2017/4/9.
  */
-@RestController
-@RequestMapping(value = "/users")
+@Controller
 public class MyRestController {
 
-    @RequestMapping(value = "/{user}/customers", method = RequestMethod.GET)
+    @Value("${application.hello:Hello Angel}")
+    private String hello;
+
+    @RequestMapping(value = "/users/{user}/customers", method = RequestMethod.GET)
+    @ResponseBody
     public User getUser(@PathVariable String user) {
         return new User(user, 30);
     }
@@ -21,5 +28,11 @@ public class MyRestController {
     @RequestMapping(value = "/{user}", method = RequestMethod.DELETE)
     public User deleteUser(@PathVariable Long user) {
         return null;
+    }
+
+    @RequestMapping("/hello1")
+    public String hello(Map<String, Object> model) {
+        model.put("test", hello);
+        return "hello";
     }
 }
